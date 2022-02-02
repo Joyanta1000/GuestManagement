ca992caad6612409ba73c56105f52043097b39dc
joyanta1000 <joyanta955@gmail.com>
2022-02-02 17:45:20 +0600

    changes_2

--- resources/views/GuestManagement/another_info.blade.php
+++ resources/views/GuestManagement/another_info.blade.php
@@ -20,13 +20,57 @@
 
 <body>
     <div style="padding: 100px;">
-        <button id="add_field" class="btn btn-primary">+</button>
 
-        <div id="select_field" style="padding-top: 20px;">
-            <br>
+        <label for="">Change For Children</label>
+        <select class="form-control" name="" id="confirmation">
+            <option value="">Select</option>
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+        </select>
+
+        <br>
+
+        <button id="add_field" class="btn btn-primary hidden">+</button>
+
+<table>
+    <thead id="thead">
+        
+    </thead>
+    <div id="message"></div>
+    <tbody id="select_field">
+        
+    </tbody>
+</table>
+
+        <!-- <div id="select_field" style="padding-top: 20px;">
+            
+        </div> -->
+        <br>
+
+        <div>
+            <button id="submit" class="btn btn-success">Add Information</button>
         </div>
     </div>
     <script>
+        $('#confirmation').change(function() {
+            if ($(this).val() == 1) {
+                s = 1;
+                addField(s);
+                $('#add_field').removeClass('hidden');
+                $('#thead').html('');
+                $('#thead').append('<tr><td>Min. Age</td><td>Max. Age</td><td></td><td>Price</td><td></td></tr>');
+
+            }
+            else if($(this).val() == 0){
+                $('#add_field').addClass('hidden');
+                $('#select_field').html('');
+                $('#thead').html('');
+                $('#add_btn').addClass('hidden');
+            }
+        });
+
+
+
         var any = 1;
         var any_1 = 2;
 
@@ -60,8 +104,8 @@
 
             console.log(max, 'max1');
 
-            loop += '<div><select data-id="' + w + '" class = "select optional" id="min_' + count + '" value = "0" onclick="ch(this)" >';
-            loop += '<option>Select</option>';
+            loop += '<tr ><td><select data-id="' + w + '" class = "select optional form-control" id="min_' + count + '" value = "0" onclick="ch(this)" required>';
+            loop += '<option >0</option>';
             for (i = max; i <= 20; i++) {
                 if (i < t) {
                     loop += '<option value="' + i + '" hidden>' + i + '</option>';
@@ -70,10 +114,10 @@
                 }
             }
 
-            loop += '</select>';
+            loop += '</select></td>';
 
-            loop += '</select><select class = "" data-id="' + w_1 + '" id="max_' + count + '" value = "0" onclick="ch(this)">';
-            loop += '<option>Select</option>';
+            loop += '<td><select class = "form-control" data-id="' + w_1 + '" id="max_' + count + '" value = "0" onclick="ch(this)" required>';
+            loop += '<option>0</option>';
             for (i = max; i <= 20; i++) {
                 if (i < t) {
                     loop += '<option value="' + i + '" hidden>' + i + '</option>';
@@ -82,7 +126,7 @@
                 }
             }
 
-            loop += '</select><button id = "remove" class = "btn btn-danger">-</button></div>';
+            loop += '</select></td><td><input class="form-control" name="price[]" required/></td><td>Tk</td><td><button id = "remove" class = "btn btn-danger">-</button></td></tr>';
             this.logic += count;
             check(logic);
             // ch(logic);
@@ -102,12 +146,13 @@
         }
 
         $(document).on('click', '#remove', function() {
-            $(this).parent().remove();
+            // $('this').parent().remove();
+            $(this).closest("tr").remove();
         });
 
         var ad = 1;
 
-        
+
 
         function appendOrNot() {
             if (s == 1) {
@@ -126,79 +171,119 @@
 
         // });
 
+        var current_selected = [];
+
         function ch(identifier) {
-            
-            console.log(any, 'any');
-            console.log(any_1, 'any_1');
 
-            // var t = document.getElementById('min_' + 1).value;
-            var t = $(identifier).val();
-            var max = $(identifier).val();
-            // toC = max;
-            // addField(toC);
-            var data_id = $(identifier).data('id');
-            console.log(t, 't');
-            s = 0;
-            // addField(t);
+            var decreased_1 = parseInt(identifier.id.substring(4)) - 1;
 
-
-
-            console.log(data_id, 'data_id');
-            if (t != 'Select') {
-                array[data_id] = t;
-                array.join();
-            }
-
-
-            console.log(array, 'array');
-
-            var index = array.indexOf(t);
-            console.log(index, 'index');
-
-            var max = array.reduce(function(a, b) {
-                return Math.max(a, b);
-            }, 0);
-
-            console.log(max, 'max');
-
-            
-
-            // option list changing
-            (function($) {
-                // Populates a select drop-down with options in a list 
-                $.fn.populate = function(list) {
-                    return this.append(list.map(item => $('<option>', {
-                        text: item,
-                        value: item
-                    })));
-                };
-            })(jQuery);
             var list = [];
-            for(i=max;i<=20;i++){
-                list.push(i);
-            }
 
-            // check();
+            if ($('#min_' + identifier.id.substring(4)).val() > $('#max_' + decreased_1).val() ) {
+                // alert("Select Same As Previous Max Age");
+                $('#message').empty();
+                $('#message').append('<label style="color: red;">Select Same As Previous Max Age</label>');
+                $('#min_' + identifier.id.substring(4)).empty().populate(list)
+            } else {
 
-            // for (i = 2; i<=2; i++){
-                
-            //     $('#min_'+i).empty().populate(list);
-            //     // i = i + 1;
-            //     $('#max_'+i).empty().populate(list);
-            // }
-            console.log(identifier.id, 'id');
+                $('#message').html('');
 
-            console.log($('#'+identifier.id).val(), 'val');
 
-            
-                if($(identifier).val()<max){
+
+
+                console.log(any, 'any');
+                console.log(any_1, 'any_1');
+
+                // var t = document.getElementById('min_' + 1).value;
+                var t = $(identifier).val();
+                var max = $(identifier).val();
+                // toC = max;
+                // addField(toC);
+                var data_id = $(identifier).data('id');
+                console.log(t, 't');
+                s = 0;
+                // addField(t);
+
+
+
+                console.log(data_id, 'data_id');
+                if (t != 'Select') {
+                    array[data_id] = t;
+                    array.join();
+                }
+
+
+                console.log(array, 'array');
+
+                var index = array.indexOf(t);
+                console.log(index, 'index');
+
+                var max = array.reduce(function(a, b) {
+                    return Math.max(a, b);
+                }, 0);
+
+                console.log(max, 'max');
+
+
+
+                // option list changing
+                (function($) {
+                    // Populates a select drop-down with options in a list 
+                    $.fn.populate = function(list) {
+                        return this.append(list.map(item => $('<option>', {
+                            text: item,
+                            value: item
+                        })));
+                    };
+                })(jQuery);
+
+                for (i = max; i <= 20; i++) {
+                    list.push(i);
+                }
+
+                // check();
+
+                // for (i = 2; i<=2; i++){
+
+                //     $('#min_'+i).empty().populate(list);
+                //     // i = i + 1;
+                //     $('#max_'+i).empty().populate(list);
+                // }
+                console.log(identifier.id, 'id');
+
+                console.log($('#' + identifier.id).val(), 'val');
+
+                // if(identifier.id == "max"){
+                //     current_selected.push($('#'+identifier.id).val());
+                // }
+
+                console.log($("selector").attr("data-id"), 'data-id');
+                // var array = [];
+                if ($('#' + identifier.id).val() <= max) {
+                    if (identifier.id.substring(0, 3) == "min") {
+                        var decreased = parseInt(identifier.id.substring(4)) - 1;
+                        console.log(decreased, 'decreased');
+                        if ($('#' + identifier.id).val() < $('#max_' + decreased).val()) {
+                            var increased = parseInt(identifier.id.substring(4));
+                            console.log(increased, 'increased');
+
+                            var list = [];
+                            for (i = $('#max_' + decreased).val(); i <= 20; i++) {
+                                list.push(i);
+                            }
+
+                            $('#' + identifier.id.substring(0, 3) + "_" + increased).empty().populate(list);
+                            $('#max_' + increased).empty().populate(list);
+                        }
+                    }
+                } else {
                     // $(identifier).empty().populate(list);
-                }
-                else{
-                    $(identifier).empty().populate(list);
+                    $('#' + identifier.id).empty().populate(list);
                 }
 
-// option list changing
+
+            }
+            // option list changing
 
         }
 
