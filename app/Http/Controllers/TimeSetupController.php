0d32148dda14795f8c3ccf625d5c034afa5d1af8
joyanta1000 <joyanta955@gmail.com>
2022-04-12 11:07:12 +0600

    change of controller

--- app/Http/Controllers/TimeSetupController.php
+++ app/Http/Controllers/TimeSetupController.php
@@ -14,18 +14,18 @@ class TimeSetupController extends Controller
 
     public function create()
     {
-        $time = '12:30';
-        $to_back = '2';
-        $timing = 'hr'; // hr or min
-        $meridiem = 'PM'; // AM or PM
+        $checkin_time = '2'; // check In time
+        $meridiem = 'PM';
+        $to_back = '1';
+        $timing = 'hr';
         $timezone = 'Asia/Dhaka';
 
         date_default_timezone_set($timezone);
 
-        if (strpos($time, ':') !== false) {
-            $date_time_1 = strtotime($time);
-        } else if (strpos($time, ':') == false) {
-            $date_time_1 = strtotime($time . ':00');
+        if (strpos($checkin_time, ':') !== false) {
+            $date_time_1 = strtotime($checkin_time);
+        } else if (strpos($checkin_time, ':') == false) {
+            $date_time_1 = strtotime($checkin_time . ':00');
         }
 
         if (strpos($to_back, ':') !== false && $timing == 'hr') {
@@ -41,9 +41,9 @@ class TimeSetupController extends Controller
         $time_diff = $date_time_1 - $date_time_2;
 
         if ($meridiem == 'AM' || $meridiem == 'am') {
-            $time_diff = $time_diff + 12 * 60 * 60;
-        } else if ($meridiem == 'PM' || $meridiem == 'pm') {
             $time_diff = $time_diff;
+        } else if ($meridiem == 'PM' || $meridiem == 'pm') {
+            $time_diff = $time_diff - 12 * 60 * 60;
         }
 
         $current = Carbon::now()->format('H:i');
